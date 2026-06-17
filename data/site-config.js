0cf1c733c5471a097f533ca9f4b308ceb2a0982e
romano6379 <romano6379@gmail.com>
2026-06-17 13:37:31 +0900

    AutoPilot: update data/site-config.js

--- data/site-config.js
+++ data/site-config.js
@@ -1,1 +1,7 @@
-window.siteConfig = { "siteName": "Federal Aid Center" };
+window.siteConfig = {
+  "siteName": "Federal Aid Center",
+  "primaryColor": "#1d4ed8",
+  "bgMain": "#f8fafc",
+  "fontHeading": "'Inter'",
+  "layoutStyle": "classic"
+};
